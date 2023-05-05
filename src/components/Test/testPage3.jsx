8f1fa667a03edd35a9ee9906573326cfd9064767
zhenikhovs <zhenihov01@mail.ru>
2023-05-06 04:47:43 +0500

    Правки

--- src/components/Test/testPage3.jsx
+++ src/components/Test/testPage3.jsx
@@ -1,23 +1,12 @@
-import { useState } from 'react'
-import { Switch } from '@headlessui/react'
+import {Navbar, Button, Dropdown, Avatar} from "flowbite-react";
 
-export default function MyToggle() {
-    const [enabled, setEnabled] = useState(false)
+const testPage3 = () => {
 
     return (
-        <Switch
-            checked={enabled}
-            onChange={setEnabled}
-            className={`${
-                enabled ? 'bg-blue-600' : 'bg-gray-200'
-            } relative inline-flex h-6 w-11 items-center rounded-full`}
-        >
-            <span className="sr-only">Enable notifications</span>
-            <span
-                className={`${
-                    enabled ? 'translate-x-6' : 'translate-x-1'
-                } inline-block h-4 w-4 transform rounded-full bg-white transition`}
-            />
-        </Switch>
+        <></>
+
     )
 }
+
+
+export default testPage3;
