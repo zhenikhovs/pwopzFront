62121dac44bb84e3e695b0f0c0e29480237c9f84
zhenikhovs <zhenihov01@mail.ru>
2023-05-07 04:48:39 +0500

    Костыль, чтобы верстать локально

--- src/components/appContainers/rootApp.jsx
+++ src/components/appContainers/rootApp.jsx
@@ -1,10 +1,6 @@
 import {Outlet, useLoaderData} from "react-router-dom";
 import AppHeader from "../header/appHeader";
 import RequireAuth from "../requireAuth/requireAuth";
-import {useEffect} from "react";
-import {setCurrentUser} from "../../store/users/slice";
-import useUserService from "../../services/UserService";
-import {useDispatch} from "react-redux";
 
 const RootApp = () => {
     const { user } = useLoaderData();
